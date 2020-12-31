62ecd999170431e820a11ddebaa3ba7c13b11acc
Nabeel Ahmed <62593855+nabeelahmed1699@users.noreply.github.com>
2020-12-31 18:00:04 +0500

    Palindrom function
    this is a function to check palindrom or not.
    bool Palindrom(const char *str,int length)

---
+++ Palindrome.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+bool palindrom(const char *str,int length)
+{
+
+    for (int i = 0; i < length; i++)
+    {
+        if(str[i] != str[length - i - 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    char str[100];
+    cout<<"Enter a String to check Palindrome: ";
+    cin >> str;
+    int lenght = strlen(str);
+    cout << palindrom(str,lenght);
+}
